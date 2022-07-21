5072cf75c54a8789c8a22f7bc415a0c613830ce3
jihun-24k <gi020477@gmail.com>
2022-07-21 13:06:33 +0900

    Feat: 구구단 서블릿 추가

---
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -0,0 +1,23 @@
+package com.ll.exam;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=utf-8");
+
+
+        int i = 9;
+        resp.getWriter().append("<h1>%d단</h1>\n".formatted(i));
+    }
+}
