587af2f3cdc08e6f6a62bb97528f9bafaf42c764
jihun-24k <gi020477@gmail.com>
2022-07-21 14:15:59 +0900

    Feat: Rq 클래스 생성

---
+++ src/main/java/com/ll/exam/Rq.java
@@ -0,0 +1,37 @@
+package com.ll.exam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class Rq {
+    HttpServletRequest req;
+    HttpServletResponse resp;
+
+    Rq(HttpServletRequest req, HttpServletResponse resp) throws UnsupportedEncodingException {
+        this.req = req;
+        this.resp = resp;
+
+        // 인코딩도 Rq 생성자에서 호출
+        this.req.setCharacterEncoding("UTF-8");
+        this.resp.setCharacterEncoding("UTF-8");
+        this.resp.setContentType("text/html; charset=utf-8");
+    }
+
+    public int getIntParam(String dan, int defaultValue) {
+        String paramValue = req.getParameter(dan);
+        if (paramValue == null)
+            return defaultValue;
+        try {
+            return Integer.parseInt(paramValue);
+        } catch (NumberFormatException e){
+            return defaultValue;
+        }
+    }
+
+    public void appendBody(String formatted) throws IOException {
+        resp.getWriter().append(formatted);
+    }
+}
