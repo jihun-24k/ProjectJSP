463da67286bf1fc776ffc1d908b2a2faeb06ebce
jihun-24k <gi020477@gmail.com>
2022-07-27 08:15:58 +0900

    Feat: 폼에서 쓴 글 list 페이지에 표시

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -15,14 +15,9 @@ public class ArticleController {
     }
 
     public void showList(Rq rq) throws IOException {
-        List<ArticleDto> articleDtos = new ArrayList<>();
+        List<ArticleDto> listDtos = articleService.findAll();
 
-        articleDtos.add(new ArticleDto(4, "제목4","내용4"));
-        articleDtos.add(new ArticleDto(3, "제목3","내용3"));
-        articleDtos.add(new ArticleDto(2, "제목2","내용2"));
-        articleDtos.add(new ArticleDto(1, "제목1","내용1"));
-
-        rq.setAtt("articles", articleDtos);
+        rq.setAtt("articles", listDtos);
         rq.view("usr/article/list");
     }
 
--- src/main/java/com/ll/exam/article/ArticleRepository.java
+++ src/main/java/com/ll/exam/article/ArticleRepository.java
@@ -20,4 +20,8 @@ public class ArticleRepository {
         articleDtos.add(newDto);
         return lastId;
     }
+
+    public List<ArticleDto> findAll() {
+        return articleDtos;
+    }
 }
