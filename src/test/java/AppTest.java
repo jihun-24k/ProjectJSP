cc974fb699cf578f581a91a216cd1ad64e20cb76
jihun-24k <gi020477@gmail.com>
2022-07-28 11:28:20 +0900

    Test: BeforeEach 추가 & assertThatTest로 변경

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,19 +1,26 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.util.Ut;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
-    @Test
-    void JUnitTest(){
-        int rs = 1 + 2;
-        assertEquals(rs,3);
+    @BeforeEach
+    void beforeEach(){
+        ObjectMapper om = new ObjectMapper();
     }
 
     @Test
-    void ObjectMapperTest(){
+    void assertThatTest(){
+        int rs = 1 + 2;
+        assertThat(rs).isEqualTo(3);
+    }
+
+    @Test
+    void objectMapperTest(){
         ArticleDto articleDto = new ArticleDto(1,"제목","내용");
         String jsonStr = Ut.json.toJsonStr(articleDto, "");
         assertThat(jsonStr).isNotBlank();
