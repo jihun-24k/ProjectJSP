8523780baea485b5502ae59c50b60091d3d2256f
jihun-24k <gi020477@gmail.com>
2022-08-01 10:05:27 +0900

    Feat: URI path 설계 및 ChatController 껍데기 메소드 만들기

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -1,6 +1,7 @@
 package com.ll.exam;
 
 import com.ll.exam.article.ArticleController;
+import com.ll.exam.chat.ChatController;
 import com.ll.exam.member.MemberController;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,23 +19,42 @@ public class DispatchServlet extends HttpServlet {
 
         ArticleController articleController = new ArticleController();
         MemberController memberController = new MemberController();
+        ChatController chatController = new ChatController();
 
         switch (rq.getMethod()) {
             case "GET" -> {
                 switch (rq.getActionPath()) {
+                    // article uri
                     case "/usr/article/detail" -> articleController.showDetail(rq);
-                    case "/usr/member/login" -> memberController.showLogin(rq);
                     case "/usr/article/list" -> articleController.showList(rq);
                     case "/usr/article/write" -> articleController.showWrite(rq);
                     case "/usr/article/delete" -> articleController.showDelete(rq);
                     case "/usr/article/modify" -> articleController.showModify(rq);
                     case "/usr/article/getArticles" -> articleController.getArticles(rq);
+                    // chat uri
+                    case "/usr/chat/roomList" -> chatController.showChatRoom(rq);
+                    case "/usr/chat/room" -> chatController.showRoom(rq);
+                    case "/usr/chat/createRoom" -> chatController.showCreateRoom(rq);
+                    case "/usr/chat/modifyRoom"-> chatController.showModifyRoom(rq);
+                    // member uri
+                    case "/usr/member/login" -> memberController.showLogin(rq);
+
                 }
             }
             case "POST" -> {
                 switch (rq.getActionPath()){
+                    // article uri
                     case "/usr/article/write" -> articleController.doWrite(rq);
                     case "/usr/article/modify" -> articleController.doModify(rq);
+                    // chat uri
+                    case "/usr/chat/writeMessage" -> chatController.doWriteMsg(rq);
+                    case "/usr/chat/createRoom" -> chatController.doCreateRoom(rq);
+                    case "/usr/chat/modifyRoom" -> chatController.doModifyRoom(rq);
+                }
+            }
+            case "DELETE" ->{
+                switch (rq.getActionPath()){
+                    case "usr/chat/deleteRoom" -> chatController.deleteRoom(rq);
                 }
             }
         }
--- src/main/java/com/ll/exam/chat/ChatController.java
+++ src/main/java/com/ll/exam/chat/ChatController.java
@@ -1,4 +1,29 @@
 package com.ll.exam.chat;
 
+import com.ll.exam.Rq;
+
 public class ChatController {
+    public void showChatRoom(Rq rq) {
+    }
+
+    public void showRoom(Rq rq) {
+    }
+
+    public void showCreateRoom(Rq rq) {
+    }
+
+    public void showModifyRoom(Rq rq) {
+    }
+
+    public void doWriteMsg(Rq rq) {
+    }
+
+    public void doCreateRoom(Rq rq) {
+    }
+
+    public void doModifyRoom(Rq rq) {
+    }
+
+    public void deleteRoom(Rq rq) {
+    }
 }
